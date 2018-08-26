c0be1116fc7419a828e0c35aaa6d1dcdac5a0139
Daniel Coop <daniel.hoani@gmail.com>
2018-08-27 06:16:24 +1200

    Connects to sql server programmatically; has button for closing connection.

--- MyFirstWebApp/Identity.aspx.cs
+++ MyFirstWebApp/Identity.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,21 @@ namespace MyFirstWebApp
 {
     public partial class Identity : System.Web.UI.Page
     {
+        SqlConnection myConnection;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            myConnection = new SqlConnection("uid=Shadowman4444;" + "pwd=Password69;" + "server=techpreneurs.cbljq0uymiqe.ap-southeast-2.rds.amazonaws.com, 1433;" +
+                            "database=Techpreneurs;");
 
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
         }
 
         protected void okButton_Click(object sender, EventArgs e)
@@ -22,6 +35,11 @@ namespace MyFirstWebApp
             string result = "Hello " + firstName + " " + lastName + ".";
 
             resultLabel.Text = result;
+        }
+
+        protected void closeConnection_Click(object sender, EventArgs e)
+        {
+            myConnection.Close();
         }
     }
 }
